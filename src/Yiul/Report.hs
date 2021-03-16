76326d2a0075d27a83aaeb10a9c1528466d4cbb9
Scott Fleischman <fleischman@simspace.com>
2021-03-16 16:57:17 -0700

    Print out package stats.

--- src/Yiul/Report.hs
+++ src/Yiul/Report.hs
@@ -538,11 +538,15 @@ organizeByPackages inputPairs = do
     putStrLn "Ignoring HIE files unable to find a matching path with Main (probably due to using GHC's main-is to point to a different module other than Main)"
     mapM_ (putStrLn . unConst . fst) exeErrors
 
-  pure $
-    Map.unionWith
-      (<>)
-      exeMainMap
-      (Map.fromListWith (<>) (normalPackagePairs <> exePairs))
+  let finalMap =
+        Map.unionWith
+          (<>)
+          exeMainMap
+          (Map.fromListWith (<>) (normalPackagePairs <> exePairs))
+
+  putStrLn $ "Loaded " <> (show . length . Map.keys) finalMap <> " packages with a total of " <> (show . length . concat . Map.elems) finalMap <> " modules."
+
+  pure finalMap
 
 instance (a ~ a2, a ~ a3, a ~ a4, a ~ a5, a ~ a6, a ~ a7, a ~ a8, a ~ a9, a ~ a10, b ~ b2, b ~ b3, b ~ b4, b ~ b5, b ~ b6, b ~ b7, b ~ b8, b ~ b9, b ~ b10) => Lens.Each (a, a2, a3, a4, a5, a6, a7, a8, a9, a10) (b, b2, b3, b4, b5, b6, b7, b8, b9, b10) a b where
   each f ~(a, b, c, d, e, g, h, i, j, k) = (,,,,,,,,,) <$> f a <*> f b <*> f c <*> f d <*> f e <*> f g <*> f h <*> f i <*> f j <*> f k
